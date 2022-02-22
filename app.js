f8f3e36d612fd16a0d46ff1e35b1eb00b59a7d78
Jimmy G <neenreva@gmail.com>
2022-02-22 10:22:27 -0500

    Helper Functions

--- app.js
+++ app.js
@@ -1,55 +1,71 @@
 // Selectors
 const totalBill = document.querySelector(".bill-total");
 const tipPercent = document.querySelectorAll(".tip-percent");
-const customTip = document.querySelector(".custom-tip");
 const people = document.querySelector(".divide");
 const tipPerPerson = document.querySelector(".tip-pp");
 const totalPerPerson = document.querySelector(".total-pp");
+const customTip = document.querySelector(".custom-tip");
+const resetParams = document.querySelector(".reset");
+
+// Varibles
+let percent;
+let tip;
+let tipAmount;
+let total;
 
 //Main function for the calculations
 function calcNums() {
-  let percent = this.value * 0.01;
-  let tip = (totalBill.value * percent).toFixed(2);
-  let tipAmount = tip / people.value;
-  let total = tipAmount + totalBill.value / people.value;
-  tipPerPerson.innerHTML = `$${tipAmount.toFixed(2)}`;
-  totalPerPerson.innerHTML = `$${total.toFixed(2)}`;
-//   console.log(percent, tip, tipAmount, total);
+  if (people.value > 0) {
+    tip = (totalBill.value * percent).toFixed(2);
+    tipAmount = tip / people.value;
+    total = tipAmount + totalBill.value / people.value;
+    tipPerPerson.innerHTML = `$${tipAmount.toFixed(2)}`;
+    totalPerPerson.innerHTML = `$${total.toFixed(2)}`;
+  }
+
+  if (percent === undefined) {
+    tipPerPerson.innerHTML = "$0.00";
+    totalPerPerson.innerHTML = "$0.00";
+  }
 }
 
 //Tip buttons
-/* tipPercent.forEach((el, i) => el.addEventListener("click", function() {
-})); */
+tipPercent.forEach((btn) => {
+  btn.addEventListener("click", tipBtns);
+});
+
+// Loop through the tip buttons and add the active-btn class to the current/clicked button
+
+function tipBtns(e) {
+  percent = this.value * 0.01;
+  console.log(percent);
+  tipPercent.forEach((btn) => {
+    btn.classList.remove("active-btn");
+    e.currentTarget.classList.add("active-btn");
+    calcNums();
+  });
+}
 
 //Custom Tip
-customTip.addEventListener("keyup", calcNums);
+customTip.addEventListener("keyup", customIpt);
+// customTip.addEventListener("input", customIpt);
 
-//Refresh view when people change
+function customIpt() {
+  percent = customTip.value * 0.01;
+  calcNums();
+}
+
+//Refresh view when people or bill change
 people.addEventListener("keyup", calcNums);
-
+totalBill.addEventListener("keyup", calcNums);
 
 // Reset
-document.querySelector(".reset").addEventListener("click", function () {
+resetParams.addEventListener("click", reset);
+
+function reset() {
   totalBill.value = "";
   customTip.value = "";
   people.value = "";
   tipPerPerson.innerHTML = "$0.00";
   totalPerPerson.innerHTML = "$0.00";
-});
-
-
-
-// Loop through the buttons and add the active-btn class to the current/clicked button
-for (let i = 0; i < tipPercent.length; i++) {
-  tipPercent[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active-btn");
-
-    // If there's no active-btn class
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace("active-btn", "");
-    }
-
-    // Add the active-btn class to the current/clicked button
-    this.className += " active-btn";
-  });
 }
